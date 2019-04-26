4a9e8c1b89519a74f8b9471171abeb0a753943b1
Kostas Mamalis <kostas.mamalis@ovoenergy.com>
2019-04-26 16:50:35 +0100

    Zokrates dance

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -22,14 +22,14 @@ contract('TestERC721Mintable', accounts => {
 
         it('should get token balance', async function () { 
             const balance = await this.contract.balanceOf(accounts[10]);
-            assert.equals(balance, 1, "wrong balance");
+            assert.equal(balance, 1, "wrong balance");
         })
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
         it('should return token uri', async function () { 
             const tokenId = 11;
             const tokenURI = await this.contract.tokenURI(tokenId);
-            assert.equal(tokenURI, `http://kostas.io/${tokenId}`);
+            assert.equal(tokenURI, `https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/${tokenId}`);
         })
 
         it('should transfer token from one owner to another', async function () { 
@@ -37,7 +37,7 @@ contract('TestERC721Mintable', accounts => {
             const accountNo13 = await this.contract.ownerOf(13, {from: account_one});
             await this.contract.transferFrom(accountNo12, accountNo13, 12, {from: accountNo12 });
             const newOwner = await this.contract.ownerOf(12, {from: account_one });
-            assert.equal(newOner, accountNo13, "transferring of token was unsuccessful");
+            assert.equal(newOwner, accountNo13, "transferring of token was unsuccessful");
         })
     });
 
@@ -49,7 +49,7 @@ contract('TestERC721Mintable', accounts => {
         it('should fail when minting when address is not contract owner', async function () { 
             try {
                 await this.contract.mint(accounts[21], 21, "http://kostas.io/21", {from: account_two });
-                asset.fail();
+                assert.fail();
             } catch (e) {
                 // passed!
             }
@@ -57,7 +57,7 @@ contract('TestERC721Mintable', accounts => {
 
         it('should return contract owner', async function () { 
             const contractOwner = await this.contract.getOwner.call({from: account_one});
-            asssert.equal(contractOwner, accountOne, "contract owner is not account_one");
+            assert.equal(contractOwner, account_one, "contract owner is not account_one");
         })
 
     });
